9d5005169d202a11e2a2fa461cc7b1b93e07b790
CDaniloPS <chrissdps@gmail.com>
2024-05-22 10:26:10 -0500

    Create
    creacion e2e

---
+++ src/main/java/com/e2e/tasks/DoCompra2.java
@@ -0,0 +1,24 @@
+package com.e2e.tasks;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Performable;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Click;
+
+import static net.serenitybdd.screenplay.Tasks.instrumented;
+
+import com.e2e.ui.ProductForms;
+
+
+public class DoCompra2  implements Task {
+    public static Performable withActor(){
+        return instrumented(DoCompra2.class);
+    }
+
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+                Click.on(ProductForms.ADD_ITEM_2)
+        );
+    }
+}
